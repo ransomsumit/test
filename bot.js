f67246c108948a173e42c2070c1cc888c51b27ff
Sumit jaiswal <ransomsumit@hotmail.com>
2022-01-22 13:03:20 +0530

    Initial

---
+++ bot.js
@@ -0,0 +1,29 @@
+const TeleBot = require("telebot");
+var token = process.env.TOKEN;
+var bot;
+if(process.env.NODE_ENV === 'production') {
+    bot = new TeleBot({
+        token: token,
+        webhook: {
+            url: process.env.HEROKU_URL + bot.token
+        }
+    });
+}
+else {
+    bot = new TeleBot({
+        token: token,
+        polling: true 
+    });
+}
+
+console.log('bot server started...');
+
+bot.on(['/hello', '/start'], async (msg) => {
+    if(msg.text.toLowerCase() == '/hello' || msg.text.toLowerCase() == '/hello@any_animebot' || msg.text.toLowerCase() == '/start' || msg.text.toLowerCase() == '/start@any_animebot')
+    {
+        let text = `Howdy, ${ msg.from.first_name }. Thank you for using Animebot. Use the '/help' command to get to know about this bot better.\n\nJoin our updates and discussion Group for quick updates and fixes - @Any_Animebot_Group \n\nIf you liked this bot you can buy me a <a href="https://www.buymeacoffee.com/ransomsumit">COFFEE</a>\n\n<b>Bot Admins: @Ransom_s, @ryugaraj, @Ola_in_uber</b>`;
+        return bot.sendMessage(msg.chat.id, text, {parseMode : "HTML", replyToMessage: msg.message_id, webPreview: false });
+    }
+});
+
+module.exports = bot;
