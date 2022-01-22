7528cfb4d9d789a1b223b57de520b9b0c25beafb
Sumit jaiswal <ransomsumit@hotmail.com>
2022-01-22 14:25:01 +0530

    Fix

--- web.js
+++ web.js
@@ -2,24 +2,21 @@ var express = require('express');
 var packageInfo = require('./package.json');
 var bodyParser = require('body-parser');
 
+
 var app = express();
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  res.json({ version: packageInfo.version });
-});
-
-var server = app.listen(process.env.PORT, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Web server started at http://%s:%s', host, port);
+var server = app.listen(process.env.port || 5000, async ()=>{
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Web server started at http://%s:%s', host, port);
 });
 
 module.exports = function (bot) {
-  app.post('/' + process.env.TOKEN, function (req, res) {
-      console.log("ok");
-    bot.processUpdate(req.body);
+  app.post('/webhook/' + process.env.TOKEN, function (req, res) {
+    console.log(req.body);
+
+    
     res.sendStatus(200);
   });
 };
